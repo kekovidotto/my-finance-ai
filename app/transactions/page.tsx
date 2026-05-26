911606dbdb4c96214f569abeee7b3f10006f56a9
QUESLON VIDOTTO DE OLIVEIRA <kekovidotto@hotmail.com>
2026-05-26 10:54:29 -0300

    feat: implement transactions page, upsert server action, and subscription page skeleton

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,3 +1,6 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 import { AddTransactionButton } from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
 import { DataTable } from "../_components/ui/data-table";
@@ -5,8 +8,17 @@ import { db } from "../_lib/prisma";
 import { transactionsColumns } from "./_columns";
 
 const TransactionsPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
   //acessar as transações do meu banco de dados
-  const transactions = await db.transaction.findMany({});
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <>
       <Navbar />
